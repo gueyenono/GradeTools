995c077b865b707beaa885771f83886a0b4cee6f
gueyenono <nonoghislain@gmail.com>
2019-07-24 18:34:11 -0500

    Implemented script to dynamically determine the number of grid rows and columns in evolution of scores box

--- R/scores-evolution-module.R
+++ R/scores-evolution-module.R
@@ -18,10 +18,16 @@ scores_evolution_module <- function(input, output, session, scores){
   
   output$performance <- renderPlot({
     
-    ggplot(data = tidy_scores(), aes(x = Assessment, y = Scores)) +
+    tidy_scores() %>%
+      filter(!is.na(Scores)) %>%
+      ggplot(aes(x = Assessment, y = Scores)) +
       geom_col() +
+      # geom_point(aes(x = as.numeric(as.character(Assessment)), y = Scores)) +
+      # geom_line(aes(x = as.numeric(as.character(Assessment)), y = Scores)) +
       theme_bw() +
-      facet_wrap(~ `Assessment Types`) +
+      facet_wrap(~ `Assessment Types`, 
+                 ncol = 3, nrow = ceiling(ncol(scores())/3),
+                 scales = "free_x") +
       scale_y_continuous(breaks = seq(from = 0, to = 100, by = 10),
                          labels = seq(from = 0, to = 100, by = 10))
     
